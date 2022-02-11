493026fcfb91e4b57f55d4a4dbf1d3f19d5cb02a
Ratnadeep Bhattacharya <ratnadeep.bhattacharya1983@gmail.com>
2022-02-11 12:32:54 -0600

    +proper error handling, +tokio1 compatibility

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-const CLIENTPORT: &str = "5000";
+const CLIENTPORT: &str = "32655";
 const PROXYINPORT: &str = "62081"; // which port will the reverse proxy use for making outgoing request
 const PROXYOUTPORT: &str = "62082"; // which port the reverse proxy listens on
 
@@ -7,14 +7,27 @@ use std::{
     time::Duration,
 };
 
-use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{error, get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use futures::future;
 use reqwest::{header::HeaderMap, Client, ClientBuilder, Method, Request, Url};
+use thiserror::Error;
 
 #[derive(Clone)]
 struct OutUrl {
     url: Url,
     client: Client,
+}
+
+#[derive(Error, Debug)]
+enum HotMess {
+    #[error("Invalid length received")]
+    InvalidLength,
+    #[error("Could not fetch body: {0}")]
+    InvalidBody(String),
+    #[error("Could not copy response body: {0}")]
+    IoCopyErr(String),
+    #[error("Failed to flush the buffer: {0}")]
+    FailedBufFlush(String),
 }
 
 #[get("/")]
@@ -36,22 +49,50 @@ async fn get_out(req: HttpRequest, data: web::Data<OutUrl>) -> impl Responder {
     .unwrap();
     println!("outgoing uri: {}", uri);
 
+    let client = &data.client.clone();
     let request = Request::new(Method::GET, uri);
-    let client = &data.client;
 
-    // TODO: hot mess: needs cleaning
-    match client.execute(request).await {
-        Ok(res) => {
-            let ln = res.content_length().unwrap() as usize;
-            let content = &res.bytes().await.unwrap()[..];
-            let mut w = BufWriter::new(vec![0u8; ln]);
-            let mut r = BufReader::new(content);
-            io::copy(&mut r, &mut w).unwrap();
-            let body = w.into_inner().unwrap();
-            HttpResponse::Ok().body(body)
+    let handle = async_std::task::spawn(client.execute(request));
+    let res = match handle.await {
+        Ok(res) => res,
+        Err(e) => {
+            return HttpResponse::InternalServerError()
+                .body(format!("Error requesting path: {}", e))
         }
-        Err(e) => HttpResponse::InternalServerError().body(format!("Error requesting path: {}", e)),
+    };
+
+    let ln = match res.content_length() {
+        Some(ln) => ln as usize,
+        None => return HttpResponse::from(error::ErrorInternalServerError(HotMess::InvalidLength)),
+    };
+    let content = match res.bytes().await {
+        Ok(b) => b,
+        Err(e) => {
+            return HttpResponse::from(error::ErrorInternalServerError(HotMess::InvalidBody(
+                e.to_string(),
+            )))
+        }
+    };
+    let mut w = BufWriter::new(vec![0u8; ln]);
+    let mut r = BufReader::new(&content[..]);
+    match io::copy(&mut r, &mut w) {
+        Ok(n) if n == 0 => return HttpResponse::Ok().body("End of stream"),
+        Ok(_) => {}
+        Err(e) => {
+            return HttpResponse::from(error::ErrorInternalServerError(HotMess::IoCopyErr(
+                e.to_string(),
+            )))
+        }
     }
+    let body = match w.into_inner() {
+        Ok(body) => body,
+        Err(e) => {
+            return HttpResponse::from(error::ErrorInternalServerError(HotMess::FailedBufFlush(
+                e.to_string(),
+            )))
+        }
+    };
+    HttpResponse::Ok().body(body)
 }
 
 #[actix_web::main]
@@ -67,7 +108,7 @@ async fn main() -> io::Result<()> {
         // .connect_timeout(Duration::new(150, 0))
         .connection_verbose(true)
         .build()
-        .unwrap();
+        .expect("Failed here");
 
     let redirect_url = OutUrl {
         url: Url::parse(&format!("http://localhost:{}", CLIENTPORT)).unwrap(),
@@ -87,6 +128,7 @@ async fn main() -> io::Result<()> {
         .bind(format!("127.0.0.1:{}", PROXYINPORT))?
         .run();
 
+    println!("request works");
     future::try_join(s_in, s_out).await?;
     Ok(())
 }
